83a4e37dd3a7584e3445b07768927a56a6364a3b
vah-ram <vahram.s.ghazaryan@gmail.com>
2025-07-17 10:18:42 +0400

    hi

--- js/index.js
+++ js/index.js
@@ -1,7 +1,9 @@
 
 const menuBtn = document.querySelector('.menuBtn');
 const leftMainHeader = document.querySelector('.leftMainHeader');
-const exitBtn = document.querySelector('.exitDiv')
+const exitBtn = document.querySelector('.exitDiv');
+const phoneNumber = document.querySelector('.topMainHeader ul .numberLi');
+const phoneNumber2 = document.querySelector('#phoneDiv .phoneNumber');
 
 menuBtn.addEventListener('click', () => {
     leftMainHeader.classList.add('view')
@@ -9,4 +11,12 @@ menuBtn.addEventListener('click', () => {
 
 exitBtn.addEventListener('click', () => {
     leftMainHeader.classList.remove('view')
+});
+
+phoneNumber.addEventListener('click', () => {
+    navigator.clipboard.writeText(phoneNumber.innerText)
+});
+
+phoneNumber2.addEventListener('click', () => {
+    navigator.clipboard.writeText(phoneNumber2.innerText)
 });
